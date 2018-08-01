7e08fa6d76c37571aa6074383249c613c07628fc
Mark Harmon <MHarmon@appriss.com>
2018-08-01 17:21:37 +0100

    Work in progress.

---
+++ FlatDb.Core/LocalFilesFlatStore.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace FlatDb.Core
+{
+    class LocalFilesFlatStore<TIdentifier, T> : IFlatStore<TIdentifier, T>
+    {
+        private const string ItemFileExtension = "json";
+        private readonly IFileProvider _fileProvider;
+
+        public LocalFilesFlatStore(IFileProvider fileProvider)
+        {
+            _fileProvider = fileProvider;
+        }
+
+        public Type ElementType => throw new NotImplementedException();
+
+        public Expression Expression => throw new NotImplementedException();
+
+        public IQueryProvider Provider => throw new NotImplementedException();
+
+        public void Delete(TIdentifier identifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Load(TIdentifier identifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Save(T item, Expression<Func<T, TIdentifier>> identifier)
+        {
+            // TODO: Lock file.
+            // TODO: Update an index.
+            var itemType = typeof(T).Name.ToLower();
+            var identifierValue = identifier.Compile()(item);
+
+            File.WriteAllText($"/{itemType}/{identifierValue}.{ItemFileExtension}", JsonConvert.SerializeObject(item));
+
+            return item;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
